a6450638b12fdd6be2ba581e3d76c2d7380ccc92
aleksma <aleksma99@yandex.ru>
2019-10-18 20:47:26 +0300

    Scripted auto-commit on change (2019-10-18 20:47:26) by gitwatch.sh

--- src/main/java/SparkAirportApp.java
+++ src/main/java/SparkAirportApp.java
@@ -1,6 +1,8 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class SparkAirportApp {
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class SparkAirportApp {
         JavaRDD<String> fligtsLines = sc.textFile(args[0]);
         JavaRDD<String> airportsLines = sc.textFile(args[1]);
 
-        
+        JavaPairRDD<Tuple2<Integer, Integer>, >
 
     }
 }
