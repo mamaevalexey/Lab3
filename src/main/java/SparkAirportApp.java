a21d06cc71b6061aad299b85b904ce02dcd87cd1
aleksma <aleksma99@yandex.ru>
2019-10-18 21:43:25 +0300

    Scripted auto-commit on change (2019-10-18 21:43:25) by gitwatch.sh

--- src/main/java/SparkAirportApp.java
+++ src/main/java/SparkAirportApp.java
@@ -8,13 +8,13 @@ public class SparkAirportApp {
     private static final int AIRPORT_CODE_INDEX = 0;
     private static final int AIRPORT_DESCRIPTION_INDEX = 1;
 
-    private static final int DEST_AIRPORT_INDEX = 14;
-    private static final int DEST_AIRPORT_INDEX = 14;
-    private static final int DELAY_INDEX = 18;
-    private static final int CANCELLED_INDEX = 19;
+    private static final int FLIGHT_ORIGIN_AIRPORT_INDEX = 11;
+    private static final int FLIGHT_DEST_AIRPORT_INDEX = 14;
+    private static final int FLIGHT_DELAY_INDEX = 18;
+    private static final int FLIGHT_CANCELLED_INDEX = 19;
 
-    private static final String AIRPORT_ID_COLUMN_NAME = "Code";
-    private static final String DEST_AIRPORT_COLUMN_NAME = "DEST_AIRPORT_ID";
+    private static final String AIRPORT_CODE_COLUMN_NAME = "Code";
+    private static final String FLIGHT_DEST_AIRPORT_COLUMN_NAME = "DEST_AIRPORT_ID";
 
     public static void main(String[] args) {
 
@@ -22,17 +22,17 @@ public class SparkAirportApp {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> flightsLines = sc.textFile(args[0]);
-        JavaRDD<String[]> flightsLinesParsed = flightsLines.map(line ->
-                line.replaceAll("\"", "").
-                        split(","));
-        flightsLinesParsed = flightsLinesParsed.filter(col -> !col[DEST_AIRPORT_INDEX].equals(DEST_AIRPORT_COLUMN_NAME));
+        JavaRDD<String[]> flightsLinesParsed = flightsLines
+                .map(line -> line.replaceAll("\"", "").split(","));
+        flightsLinesParsed = flightsLinesParsed
+                .filter(col -> !col[FLIGHT_DEST_AIRPORT_INDEX].equals(FLIGHT_DEST_AIRPORT_COLUMN_NAME));
 
 
         JavaRDD<String> airportsLines = sc.textFile(args[1]);
-        JavaRDD<String[]> airportsLinesParsed = airportsLines.map(line ->
-                line.replaceAll("\"", "").
-                        split(","));
-        airportsLinesParsed = airportsLinesParsed.filter(col -> !col[0].equals("Code"));
+        JavaRDD<String[]> airportsLinesParsed = airportsLines
+                .map(line -> line.replaceAll("\"", "").split(","));
+        airportsLinesParsed = airportsLinesParsed
+                .filter(col -> !col[AIRPORT_CODE_INDEX].equals(AIRPORT_CODE_COLUMN_NAME));
 
 
         JavaPairRDD<Tuple2<String, String>, Integer> flightStatPairs = flightsLines.mapToPair(
