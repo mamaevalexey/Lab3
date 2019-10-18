3dcbe763d0dc91edb14c15aea25d85faf678608e
aleksma <aleksma99@yandex.ru>
2019-10-18 20:31:59 +0300

    Scripted auto-commit on change (2019-10-18 20:31:59) by gitwatch.sh

--- src/main/java/SparkAirportApp.java
+++ src/main/java/SparkAirportApp.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkAirportApp {
@@ -7,7 +8,7 @@ public class SparkAirportApp {
         SparkConf conf = new SparkConf().setAppName("Lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-                
+        JavaRDD<String> fligtsFile =         
 
     }
 }
