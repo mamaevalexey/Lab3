4c4300f198e309140c7286f12daafc4e58e87769
aleksma <aleksma99@yandex.ru>
2019-10-18 20:26:26 +0300

    Scripted auto-commit on change (2019-10-18 20:26:26) by gitwatch.sh

--- src/main/java/SparkJoinApp.java
+++ src/main/java/SparkJoinApp.java
@@ -1,5 +1,12 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class SparkJoinApp {
     public static void main(String[] args) {
+
+        SparkConf conf = new SparkConf().setAppName("Lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
         
     }
 }
