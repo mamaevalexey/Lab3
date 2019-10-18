8c82031afa7a583b095e963dda1698450817b417
aleksma <aleksma99@yandex.ru>
2019-10-18 20:57:00 +0300

    Scripted auto-commit on change (2019-10-18 20:57:00) by gitwatch.sh

--- src/main/java/FlightStatsKey.java
+++ src/main/java/FlightStatsKey.java
@@ -6,6 +6,38 @@ public class FlightStatsKey implements Serializable {
     private int cancelledFlights;
     private int flights;
 
+    public float getMaxDelay() {
+        return maxDelay;
+    }
+
+    public int getLateFlights() {
+        return lateFlights;
+    }
+
+    public int getCancelledFlights() {
+        return cancelledFlights;
+    }
+
+    public int getFlights() {
+        return flights;
+    }
+
+    public void setMaxDelay(float maxDelay) {
+        this.maxDelay = maxDelay;
+    }
+
+    public void setLateFlights(int lateFlights) {
+        this.lateFlights = lateFlights;
+    }
+
+    public void setCancelledFlights(int cancelledFlights) {
+        this.cancelledFlights = cancelledFlights;
+    }
+
+    public void setFlights(int flights) {
+        this.flights = flights;
+    }
+
     @Override
     public String toString() {
         return super.toString();
